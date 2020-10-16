16ed4d8eee253e512bba6bcccd41d394994aac08
KPMGE <jesuskevin254@gmail.com>
2020-10-16 18:46:19 -0300

    Adding first script with Shell script

---
+++ first-script.sh
@@ -0,0 +1,14 @@
+#!usr/bin
+
+# displaying hello world on the screen of different ways
+echo "Hello      World"      
+echo "Hello World"
+echo "Hello * World"
+echo Hello * World
+echo Hello      World
+echo "Hello" World
+echo Hello "     " World
+echo "Hello "*" World"
+echo `hello` world
+echo 'hello' world
+
