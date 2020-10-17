53108ad73be700631458f71c437a84cdff94401f
KPMGE <jesuskevin254@gmail.com>
2020-10-16 23:25:25 -0300

    Adding example of how to make a script able to wait some seconds before execute a new command

---
+++ sleep.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+
+# we can to wait some seconds before to execute a new command, for it
+# we can use the sleep command, for it, is assigned a value in seconds
+
+echo "Hello, i'm here"
+sleep 10    # sleep 10 seconds
+echo "hello, i'm here again, after 10 seconds"
